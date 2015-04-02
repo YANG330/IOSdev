92686a6501a6d466a713a49df1d0fec518e4159b
YANG330 <yangyiyang33@gmail.com>
2015-04-02 21:00:40 +0300

    IOSdeve
    Ios developing

---
+++ Yang ios app/ios app Dev/ios app Dev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios app Dev
+//
+//  Created by kasutaja on 12/03/15.
+//  Copyright (c) 2015 Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
